ba4f96594f3581f29c116f2b608a8672802a57f2
LogEman <thelogankessler@gmail.com>
2025-02-04 21:29:16 -0600

    Basic sim logic that should work with a button probably

--- src/main/java/frc/robot/subsystems/ArmClimberSim.java
+++ src/main/java/frc/robot/subsystems/ArmClimberSim.java
@@ -12,21 +12,40 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmClimberSim extends SubsystemBase {
+  double currentAngle;
+  double anglePerIteration;
+  double maxAngle;
+  double minAngle;
+
   /** Creates a new ArmClimber. */
-  public ArmClimberSim() {}
+  public ArmClimberSim() {
+
+    currentAngle = 90;
+    maxAngle = 90;
+    minAngle = 0;
+    anglePerIteration = 10;
+  }
 
   Rotation3d armRotation = new Rotation3d(0, 0, 0);
+
 
   @Override
   public void periodic() {
     Logger.recordOutput("ZeroedComponentPoses", new Pose3d[] {new Pose3d()});
+    // Logger.recordOutput("FinalComponentPoses", new Pose3d[]{
+    //   new Pose3d(-0.309, -0.015, 0.135, new Rotation3d(0.0, Math.sin(Timer.getTimestamp()) - 1.0, 0.0))
+    // });
     Logger.recordOutput("FinalComponentPoses", new Pose3d[]{
-      new Pose3d(-0.309, -0.015, 0.135, new Rotation3d(0.0, Math.sin(Timer.getTimestamp()) - 1.0, 0.0))
+      new Pose3d(-0.309, -0.015, 0.135, new Rotation3d(0.0, Math.toRadians(currentAngle), 0.0))
     });
+    //runArmMotor();
     // This method will be called once per scheduler run
   }
 
   public void runArmMotor(){
-    
+    currentAngle += Math.min(Math.max(currentAngle + anglePerIteration, minAngle), maxAngle);
+      
+
+
   }
 }
