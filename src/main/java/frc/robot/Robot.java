9600ef36ff8592dfbbda6bdce5ae3b5b4438104c
Taaakkechan <junjun.brewer@gmail.com>
2025-02-06 21:02:05 -0600

    Made limelight recognize apriltag and make LED blink

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,6 +19,7 @@ import com.ctre.phoenix6.swerve.SwerveModuleConstants.SteerMotorArrangement;
 import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.LimelightHelpers.LimelightResults;
 import frc.robot.generated.TunerConstants;
 import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
@@ -139,7 +140,15 @@ public class Robot extends LoggedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    double lNum = LimelightHelpers.getFiducialID("");
+    if(lNum == 8)
+    {
+      LimelightHelpers.setLEDMode_ForceBlink("");
+    } else {
+      LimelightHelpers.setLEDMode_ForceOn("");
+    }
+  }
 
   /** This function is called once when test mode is enabled. */
   @Override
--- src/main/java/frc/robot/subsystems/drive/Limelight.java
+++ src/main/java/frc/robot/subsystems/drive/Limelight.java
@@ -1,3 +1,8 @@
+package frc.robot.subsystems.drive;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 public class Limelight {
     
 }
