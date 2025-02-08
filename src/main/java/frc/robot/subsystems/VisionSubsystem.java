49abfeca597be7c9362d656916aeeb3f8e1d7edc
Miles Poland <miles.h.poland@gmail.com>
2025-02-08 14:55:43 -0600

    Super simple vision subsystem

---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.LimelightHelpers;
+
+public class VisionSubsystem extends SubsystemBase {
+
+  public LimelightHelpers.LimelightTarget_Fiducial AprilTag;
+  public VisionSubsystem() {
+    AprilTag = new LimelightHelpers.LimelightTarget_Fiducial();
+    LimelightHelpers.setPipelineIndex("", 0); // 0 is 2D Fiducials, 1 is 3D fiducials
+  }
+
+
+  @Override
+  public void periodic() {
+    if(!LimelightHelpers.getTV("")){
+        AprilTag = null; // TODO: Better error case
+        return;
+    }
+    AprilTag = LimelightHelpers.getLatestResults("").targets_Fiducials[0];
+    
+    // This method will be called once per scheduler run
+  }
+}
--- src/main/java/frc/robot/subsystems/drive/Limelight.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems.drive;
-
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class Limelight {
-    
-}
