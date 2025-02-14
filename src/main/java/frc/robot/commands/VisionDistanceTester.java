2b869d460cf903935635207d56d38690626c9673
Miles Poland <miles.h.poland@gmail.com>
2025-02-13 20:20:35 -0600

    added note

--- src/main/java/frc/robot/commands/VisionDistanceTester.java
+++ src/main/java/frc/robot/commands/VisionDistanceTester.java
@@ -12,6 +12,7 @@ import frc.robot.subsystems.drive.Drive;
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class VisionDistanceTester extends Command {
   /** Creates a new VisionDistanceTester. */
+  // UNTESTED
   public VisionSubsystem m_vis;
   public Drive m_drive;
   double propOffset;
