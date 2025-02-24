45a941d038f00df27824513ab6152a0a0ed7cbaf
LogEman <thelogankessler@gmail.com>
2025-02-24 17:31:31 -0600

    ADDED EXAMPLES

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,12 +7,28 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.MotorExamples.RunMotor;
+import frc.robot.subsystems.Motors.GenericMotor;
+import frc.robot.subsystems.Motors.SparkFlexMotor;
+import frc.robot.subsystems.Motors.TalonFXMotor;
 public class RobotContainer {
+
+  GenericMotor talonExampleMotor;
+  GenericMotor revExampleMotor;
+  
   public RobotContainer() {
+
+    talonExampleMotor = new TalonFXMotor(0, "rio");
+    revExampleMotor = new SparkFlexMotor(1);
+
     configureBindings();
   }
 
   private void configureBindings() {
-   
+   new Trigger(() -> true)
+      .whileTrue(new RunMotor(revExampleMotor, 0.5));
+  
+   new Trigger(() -> true)
+      .whileTrue(new RunMotor(talonExampleMotor, 0.5));
   }
 }
