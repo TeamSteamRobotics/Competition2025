c4b0f7cce05780d08b78a61fb84720a06401b618
LogEman <thelogankessler@gmail.com>
2025-02-25 10:03:42 -0600

    Added Intake

---
+++ src/main/java/frc/robot/commands/Intake/Tests/RollerTest.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Tests;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSubsystem;
+
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class RollerTest extends Command {
+  private  IntakeSubsystem m_IntakeSubsystem; // The subsystem this command controls.
+  private double m_targetSpeed;
+  //private int counter;
+  /** Creates a new Intake. */
+  public RollerTest(IntakeSubsystem intakeSubsystem, double targetSpeed) {
+    m_IntakeSubsystem = intakeSubsystem;
+    m_targetSpeed = targetSpeed;
+    
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_IntakeSubsystem.roller(m_targetSpeed);
+    
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_IntakeSubsystem.roller(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,110 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.Newton;
+
+import org.littletonrobotics.junction.Logger;
+
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.motorIdConstants;
+import frc.robot.Constants.Intake.*;
+import frc.robot.subsystems.Motors.GenericMotor;
+import frc.robot.subsystems.Motors.TalonFXMotor;
+import frc.robot.subsystems.Motors.SparkFlexMotor;
+
+
+public class IntakeSubsystem extends SubsystemBase {
+  /** Creates a new IntakeSubsystem. */
+  
+  // Motor controllers for the intake roller and pivot mechanism
+
+  GenericMotor rollerMotor;
+  GenericMotor pivotMotor;
+  
+  // PID controllers for controlling the roller and pivot speeds
+  PIDController rollerPid = new PIDController(Roller.kP, Roller.kI, Roller.kD);
+  PIDController pivotPid = new PIDController(Pivot.kP, Pivot.kI, Pivot.kD);
+
+  // Variables to store target speeds
+  double m_targetRollerSpeed;
+  double m_targetPivotPosition;
+
+  public IntakeSubsystem() {
+    rollerMotor = new TalonFXMotor(motorIdConstants.rollerId, "rio");
+    pivotMotor = new SparkFlexMotor(motorIdConstants.pivotId);
+    
+    // Setting initial PID tolerances
+    rollerPid.setTolerance(Roller.tolerance);
+    pivotPid.setTolerance(Pivot.tolerance);
+
+  }
+
+  /**
+   * Controls the roller motor using a PID controller.
+   * @param targetRollerSpeed The desired speed for the roller motor.
+   * @return true if the roller motor speed is at the target setpoint.
+   */
+  public boolean rollerPID(double targetRollerSpeed) {
+    m_targetRollerSpeed = targetRollerSpeed;
+
+    // Compute the PID output for the roller motor based on encoder velocity feedback
+    double pidOutputRoller = rollerPid.calculate(rollerMotor.getVelocity(), m_targetRollerSpeed);
+    
+    // Apply the computed PID output to the roller motor
+    rollerMotor.set(pidOutputRoller);
+  
+    // Return whether the PID controller has reached the setpoint
+    return pivotPid.atSetpoint();
+  }
+
+  public void roller(double rollerSpeed){
+    m_targetRollerSpeed = rollerSpeed;
+    rollerMotor.set(m_targetRollerSpeed);
+  }
+  
+  
+  /**
+   * Controls the pivot motor using a PID controller.
+   * @param targetPivotPosition The desired speed for the pivot motor.
+   * @return true if the pivot motor speed is at the target setpoint.
+   */
+  public boolean pivotPID(double targetPivotPosition) {
+    m_targetPivotPosition = targetPivotPosition;
+
+    // Compute the PID output for the pivot motor based on encoder velocity feedback
+    double pidOutputPivot = pivotPid.calculate(pivotMotor.getAbsolutePosition(), m_targetPivotPosition);
+    
+    // Apply the computed PID output to the pivot motor
+    pivotMotor.set(pidOutputPivot);
+
+    // Return whether the PID controller has reached the setpoint
+    return pivotPid.atSetpoint();
+  }
+
+  public void pivot(double pivotSpeed){
+    pivotMotor.set(pivotSpeed);
+  }
+
+  @Override
+  public void periodic() {
+    Logger.recordOutput("Intake/FinalComponentPoses", new Pose3d[]{
+      new Pose3d(-0.25, -0.3, 0.16, new Rotation3d(Math.toRadians((pivotMotor.getPosition() / 9)* 360), 0.0, 0.0))
+    });
+    // This method will be called once per scheduler run
+  }
+}
